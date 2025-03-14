6d38b4fe35aad419562685fbe215f61ffc4879b0
Phillip <108208254+roachboisss@users.noreply.github.com>
2025-03-14 13:38:55 -0400

    Update info.js

--- info.js
+++ info.js
@@ -1,4 +1,4 @@
-const txt = '{"name":"Business Name", "number":"(123) 456-7890", "year":"2025", "email":"businessemail@gmail.com", "about":"About the business goes here", "owner":"Owners name"}'
+const txt = '{"name":"Business Name", "number":"(123) 456-7890", "year":"2025", "email":"businessemail@gmail.com", "about":"About the business goes here", "owner":"Owners name", "mission":"Mission statement"}'
 //VARIABLES
 //name - name of business
 //number - business number
@@ -6,6 +6,7 @@ const txt = '{"name":"Business Name", "number":"(123) 456-7890", "year":"2025", 
 //email - business email
 //about - about the business
 //owner - name of the company owner
+//mission - the mission statement
 const obj = JSON.parse(txt);
 document.getElementById("name").innerHTML = obj.name + " ";
 document.getElementById("number").innerHTML = obj.number + " ";
@@ -13,3 +14,4 @@ document.getElementById("year").innerHTML = obj.year + " ";
 document.getElementById("email").innerHTML = obj.email + " ";
 document.getElementById("about").innerHTML = obj.about + " ";
 document.getElementById("owner").innerHTML = obj.owner + " ";
+document.getElementById("mission").innerHTML = obj.mission + " ";
