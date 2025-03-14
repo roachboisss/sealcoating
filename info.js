f27058f3b460b7dd97ff808c9cfb49b2a55bc9e6
Phillip <108208254+roachboisss@users.noreply.github.com>
2025-03-14 13:28:56 -0400

    Update info.js

--- info.js
+++ info.js
@@ -1,7 +1,13 @@
-const txt = '{"name":"Business Name", "number":"(123) 456-7890", "year":"2025", "email":"businessemail@gmail.com"}'
-
+const txt = '{"name":"Business Name", "number":"(123) 456-7890", "year":"2025", "email":"businessemail@gmail.com", "about":"About the business goes here"}'
+//VARIABLES
+//name - name of business
+//number - business number
+//year - current year
+//email - business email
+//about - about the business
 const obj = JSON.parse(txt);
 document.getElementById("name").innerHTML = obj.name + " ";
 document.getElementById("number").innerHTML = obj.number + " ";
 document.getElementById("year").innerHTML = obj.year + " ";
 document.getElementById("email").innerHTML = obj.email + " ";
+document.getElementById("about").innerHTML = obj.about + " ";
