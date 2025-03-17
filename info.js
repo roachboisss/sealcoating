f3e01282fe782d33939f54738432a4c31abb8886
Phillip <108208254+roachboisss@users.noreply.github.com>
2025-03-17 11:24:07 -0400

    Update info.js

--- info.js
+++ info.js
@@ -1,11 +1,11 @@
 const businessInfo = {
-  "name": "Business Name", 
-  "number": "(123) 456-7890", 
-  "year": "2025", 
-  "email": "businessemail@gmail.com", 
-  "about": "About the business goes here", 
-  "owner": "Owners name", 
-  "mission": "Mission statement"
+  "name": "Business Name",                  //Business name
+  "number": "(123) 456-7890",               //Business number
+  "year": "2025",                           //Current year
+  "email": "businessemail@gmail.com",       //Business email
+  "about": "About the business goes here",  //About business for about page
+  "owner": "Owners name",                   //Business owners name (owns business nunber and emal)
+  "mission": "Mission statement"            //Business mission statement
 };
 
 // Wait for the DOM to be fully loaded before trying to access elements
