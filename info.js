8fbc5eefbe66d58bc304291d72b9f4e4d46afbe1
Phillip <108208254+roachboisss@users.noreply.github.com>
2025-03-17 12:56:50 -0400

    Update info.js

--- info.js
+++ info.js
@@ -1,5 +1,5 @@
 const businessInfo = {
-  "name": "One man sealcoating",                     //Business name
+  "name": "Business name",                     //Business name
   "number": "(123) 456-7890",                  //Business number
   "year": "2025",                              //Current year
   "businessemail": "businessemail@gmail.com",  //Business email
